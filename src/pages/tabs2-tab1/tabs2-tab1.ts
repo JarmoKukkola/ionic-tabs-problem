e2b3d360c56a4304c8779900702975188b7f842f
Jarmo Kukkola <jarmo.kukkola@gmail.com>
2017-10-16 20:23:09 +0200

    d

--- src/pages/tabs2-tab1/tabs2-tab1.ts
+++ src/pages/tabs2-tab1/tabs2-tab1.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, App } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,6 +8,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class Tabs2Tab1Page {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  previousPage='Tabs2Page';
+  
+    constructor(public navCtrl: NavController, public app:App) {
+    }
+  
+    one(){
+      this.navCtrl.pop();
+    }
+  
+    two(){
+      this.navCtrl.setRoot(this.previousPage);
+    }
+  
+    three(){
+      this.app.getRootNavs()[0].pop();
+    }
+  
+    four(){
+      this.app.getRootNavs()[0].setRoot(this.previousPage);
+    }
+  
+    five(){
+      this.app.getRootNav().pop();
+    }
+    
+    six(){
+      this.app.getRootNav().setRoot(this.previousPage);
+    }
+  
+    seven(){
+      this.app.getActiveNav().pop();
+    }
+  
+    eight(){
+      this.app.getActiveNav().setRoot(this.previousPage);
+    }
+  
+    nine(){
+      this.app.getActiveNavs()[0].pop()
+    }
+  
+    ten(){
+      this.app.getActiveNavs()[0].setRoot(this.previousPage);
+    }
 }
